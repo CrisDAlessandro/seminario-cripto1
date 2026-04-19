45987cb2cfcb0ecb772b8bd2e72a9aff14c05c6c
Cristian <91854757+CrisDAlessandro@users.noreply.github.com>
2026-04-19 19:29:05 -0300

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, useRef } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
